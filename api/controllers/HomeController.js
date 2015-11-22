06625a3d8127927ff4d57b30949f2af2d6c29d00
Madai Arteaga <artmadai@hotmail.com>
2015-11-22 16:52:39 -0500

    Más cambios y creación de elementos para el registro
    Falta añadir, subir imagen!! :facepunch:

---
+++ api/controllers/HomeController.js
@@ -0,0 +1,11 @@
+/**
+ * HomeController
+ *
+ * @description :: Server-side logic for managing Homes
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
